ef150f9222038a30acdaeec63128e4f735cfbdea
Elvis Cai <elvis@Elviss-MBP.hitronhub.home>
2023-09-05 23:18:18 -0700

    configured run.R

--- run.R
+++ run.R
@@ -1,7 +1,29 @@
-ion_count <- readxl::read_xlsx("data/Datacorrected_20230419_ET.xlsx",
+
+
+# First time run
+
+# install.packages("devtools")
+# library("devtools")
+# load_all()
+
+
+if (!dir.exists("data/")){
+  print("Add the data file to the data folder")
+  dir.create("data/")
+}
+
+if (!dir.exists("output/")){
+  dir.create("output/")
+}
+
+
+file_loc <- "data/Datacorrected_20230419_ET.xlsx"
+
+
+ion_count <- readxl::read_xlsx(file_loc,
                                range = "A40:Y75")
 
-iso_abun <- readxl::read_xlsx("data/Datacorrected_20230419_ET.xlsx",
+iso_abun <- readxl::read_xlsx(file_loc,
                               range = "A78:Z284")
 
 
