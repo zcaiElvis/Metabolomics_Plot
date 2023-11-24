29e06c5ccfbd625f5106dd50598326632ea01bc5
Elvis Cai <elvis@Elviss-MBP.hitronhub.home>
2023-11-23 21:32:12 -0800

    added more instruction

--- run.R
+++ run.R
@@ -1,5 +1,6 @@
 #### First time run R
-install.packages("devtools")
+
+# install.packages("devtools") #uncomment and run this if the next line gives error
 library("devtools")
 load_all()
 
@@ -17,8 +18,8 @@ if (!dir.exists("output/")){
 #### File location & processing
 file_loc <- "data/Datacorrected_20231110-ET.xlsx" # data location here
 
-ion_table_loc <- "A43:AB81"
-iso_table_loc <- "A84:AC305"
+ion_table_loc <- "A43:AB81" # top left (cell with value "Fragment") to bottom right
+iso_table_loc <- "A84:AC305" # top left (cell with value "Ion") to bottom right
 
 ion_count <- readxl::read_xlsx(file_loc,
                                range = ion_table_loc)
@@ -33,7 +34,18 @@ View(iso_abun)
 
 #### Generate and save plots
 
-xtick <- c("10 EV", "10 Cre", "1 EV", "1 Cre", "U13C 10 EV", "U13C 10 Cre", "U13C 1 EV", "U13C 1 Cre")
-
 multiplied_df <- plot_abundance(ion_count, iso_abun, group_length = 8, total_length = 24,
-               removed_cols = c(17, 21, 26, 27, 28), save_loc = "Nov19-1/")
+                                removed_cols = c(17, 21, 26, 27, 28),
+                                save_loc = "Nov19-1/")
+
+
+#### Generate plots with x ticks
+
+xtick <- c("10 EV", "10 Cre", "1 EV", "1 Cre", "U13C 10 EV", "U13C 10 Cre", "U13C 1 EV", "U13C 1 Cre")
+multiplied_df <- plot_abundance(ion_count, iso_abun, group_length = 8, total_length = 24,
+                                xtick = xtick, removed_cols = c(17, 21, 26, 27, 28),
+                                save_loc = "Nov19-1/")
+
+#### For full usage of the function, call
+?plot_abundance
+
